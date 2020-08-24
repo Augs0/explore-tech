f03596abfdfd01eb70098a43d146eb035d2503b8
August Aldred <augustaldred@bpp.com>
2020-08-24 11:11:13 +0100

    version one of site complete

--- script.js
+++ script.js
@@ -11,8 +11,8 @@ codingJobs = [
     },
     {
         "role": "Fullstack Developer",
-        "desc": "Fullstack development combines both frontend development and backend development. You may use both to a similar degree or potentially use more of one skillset than the other. This can vary from project to project or sector to sector.",
-        "skills": "Basically, you are looking to have a mix of skills combining what has been outlined in frontend and backend developemnt. What skills you choose to focus on depends on your interests and goals."
+        "desc": "Fullstack development combines both frontend development and backend development. You may use both to a similar degree or potentially use more of one skill-set than the other. This can vary from project to project or sector to sector.",
+        "skills": "Basically, you are looking to have a mix of skills combining what has been outlined in frontend and backend development. What skills you choose to focus on depends on your interests and goals."
     },
     {
         "role": "Mobile app developer",
@@ -24,7 +24,7 @@ codingJobs = [
 creativeJobs = [
     {
         "role": "UI/UX designer",
-        "desc": "You may see UI designer or UX designer listed separately or sometimes the two are combined. UI stands for 'user interface' whereas UX stands for 'user experience'. The two often overlap and effectively what this field of work involves is designing sites and apps by creating mockups to be researched and tested, taking into account the journey the user goes on when using the platform and whether it's a good and intutitve experience for them. They may also be in charge of coming up with styling guides for developers to follow (e.g. fonts, colours, etc.).",
+        "desc": "You may see UI designer or UX designer listed separately or sometimes the two are combined. UI stands for 'user interface' whereas UX stands for 'user experience'. The two often overlap and effectively what this field of work involves is designing sites and apps by creating mockups to be researched and tested, taking into account the journey the user goes on when using the platform and whether it's a good and intuitive experience for them. They may also be in charge of coming up with styling guides for developers to follow (e.g. fonts, colours, etc.).",
         "skills": "Sketch or other wireframing tools like Adobe XD or Figma, Photoshop, Writing and research skills"
     }
 ]
@@ -33,7 +33,7 @@ safetyJobs = [
     {
         "role": "Cyber-security analyst/technician",
         "desc": "Cyber-security is a growing and in-demand part of technology that handles the security of sites, apps, databases, etc. Working in cyber-security, you're trying to keep something safe, as well as being prepared in dealing with any kind of hack or attack. Cyber-security technicians and analysts often are involved in ethical hacking, that is trying to purposefully break into systems and platforms to test how secure they are and to fix any potential problems that could lead to damage.",
-        "skills": "There are a number of certificates that may help you progress in cyber-security, such as CISSP. Languages often linked to this line of work include Python, Jvascript, SQL, PHP, C, C++ and Assembly, however, you should be familiar with what the project or your company calls for. You may want to look into Virtual Machines, effectively computers inside computers, which cyber-security technicians use for activities like examining malware and testing across operating systems."
+        "skills": "There are a number of certificates that may help you progress in cyber-security, such as CISSP. Languages often linked to this line of work include Python, Javascript, SQL, PHP, C, C++ and Assembly, however, you should be familiar with what the project or your company calls for. You may want to look into Virtual Machines, effectively computers inside computers, which cyber-security technicians use for activities like examining malware and testing across operating systems."
     }
 ]
 
@@ -41,34 +41,39 @@ safetyJobs = [
 businessJobs = [
     {
         "role": "Business intelligence analyst",
-        "desc": "description",
-        "skills": ""
+        "desc": "Business intelligence analysis is actually very data-driven work, however, this has been grouped with business-related jobs as your work and research will figure heavily into the decisions of companies and stakeholders. Your job will be to analyse data, trends, user behaviour and more to help senior staff decide what direction to take the business.",
+        "skills": "Technical skills can be very helpful in this role. You may find some individuals move into this kind of work after working in development themselves, but generally speaking, alongside the research and presentation skills required in this kind of work, business intelligence analysts may need skills in database management, languages like SQL, and understanding of related tools like PowerBI and SAS."
     },
     {
         "role": "Consultant",
-        "desc": "description",
-        "skills": ""
+        "desc": "Again, this kind of work may come after a career in technology as it will help you understand what skills companies are really looking for. Consultancy involves helping business find the best technological and software solutions for their company and situation, therefore, knowing how and why one language or framework might be chosen over another is key.",
+        "skills": "At the very least, technology and IT consultants should be aware of the market and sector they're working in, whether that's app development, cyber-security, database management - whatever it may be. Additionally, you need good interpersonal and business skills to be able to liaise with many clients and to really understand what they need."
     }
 ]
 
 dataJobs = [
     {
         "role": "Data scientist",
-        "desc": "description",
-        "skills": ""
+        "desc": "Data science at its heart is the analysis of data. Data scientists or data analysts work in a very broad range of jobs, but largely focus on data and what it means. This kind of programming is very much what people think all web development is until they start to learn more.",
+        "skills": "Programming skills are quite important in these kinds of jobs, with languages like JAVA, R, SQL, and Python being particularly popular. You may also want to research things like Big Data and Machine Learning. Having a strong understanding of mathematics and algorithms is also beneficial."
     }
 ]
 
 otherJobs = [
     {
         "role": "QA tester",
-        "desc": "description",
-        "skills": ""
+        "desc": "QA stands for 'quality assurance' and this type of work involves rigorously testing code written by developers. You may be running pre-written tests or even writing your own to make sure that the code being written works and works well.",
+        "skills": "Of course, being able to read and understand code will help you a great deal in this line of work, particularly if looking for bugs. A good understanding of version control is important as you may be involved in handling pull requests and communicating bugs and fixes with a larger team. Tools like Postman are used to test API routes. There are different kinds of QA testers out there and not all of them need code for their job - it varies. For example, QA testers exist in things like localization of video games, and these testers are looking at the output of the translations, making sure it makes sense. Sometimes QA testers in tech are also output-focused too and may not need to be as technically savvy as a developer."
     },
     {
         "role": "DevOps Engineer",
-        "desc": "description",
-        "skills": ""
+        "desc": "DevOps is a relatively new and growing field. DevOps engineers may be involved at different parts of an entire process but they basically combine technical skills with project management, often being responsible for handling the preparation and deployment of products.",
+        "skills": "Having coding knowledge can be very helpful, and you will likely need to learn how to use automation tools and system administration. Much like fullstack developers, DevOps engineers must have a very wide and varied skillset and the exact skills needed may change from project to project or job to job."
+    },
+    {
+        "role": "Cloud Engineer",
+        "desc": "You have almost certainly heard of 'the cloud' when talking about data. We often use this kind of storage on our own laptops or mobile phones. Cloud engineer is a term encompassing people ina wide variety of jobs and you may actually hear them called Solutions Architects, Cloud Developers or SysOps Engineers. These other terms point to what certification an engineer has, but they are all Cloud Engineers of some kind and their work involves creating and maintaining cloud-based applications and services.",
+        "skills": "Certifications in this field include those from AWS (Amazon Web Services), Azure (Microsoft) and GCP (Google Cloud Platform) and are a good place to start to learn more about what cloud engineering involves. You should be comfortable with APIs and how they work and some popular languages that benefit you in this field inclue Java, Python and Go, however, as usual, languages may vary from situation to situation."
     }
 ]
 
